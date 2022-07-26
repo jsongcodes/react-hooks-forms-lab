099ee8df79bbdd26c2599f4b8ed18c106abc2a01
Jasmin <ssongnari@gmail.com>
2022-07-26 16:25:43 -0400

    commit tree bc9399d27dca4099f669a8febdbc175967564933

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,30 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
-  });
+  const itemsToDisplay = items
+    // category
+    .filter(
+      (item) => selectedCategory === "All" || item.category === selectedCategory
+    )
+    // search term
+    .filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
+      <Filter
+        search={search}
+        onSearchChange={setSearch}
+        onCategoryChange={handleCategoryChange}
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
@@ -30,3 +37,44 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
+
+// import React, { useState } from "react";
+// import ItemForm from "./ItemForm";
+// import Filter from "./Filter";
+// import Item from "./Item";
+
+// function ShoppingList({ items }) {
+//   const [selectedCategory, setSelectedCategory] = useState("All");
+//   const [searchWord, setSearchWord] = useState('')
+
+//   function handleCategoryChange(event) {
+//     setSelectedCategory(event.target.value);
+//   }
+
+//   const itemsToDisplay = items.filter((item) => {
+//     if (selectedCategory === "All" && searchWord === "") return true;
+//     if (item.name.toLowerCase().includes(searchWord.toLowerCase())){
+//       if (selectedCategory === "All"){
+//        return true;
+//       }
+//       else if(item.category === selectedCategory){
+//         return true;
+//       }
+// } 
+//   });
+
+//   return (
+//     <div className="ShoppingList">
+//       <ItemForm />
+//       <Filter onCategoryChange={handleCategoryChange} onSearchChange={setSearchWord} />
+//       <ul className="Items">
+//         {itemsToDisplay.map((item) => (
+//           <Item key={item.id} name={item.name} category={item.category} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default ShoppingList;
